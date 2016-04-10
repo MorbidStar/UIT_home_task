dd26657d2e6f1120f7f77d2b906d7735f58c159a
MorbidStar <morbidforever1@rambler.ru>
2016-04-10 21:07:24 +0300

    реализовал функцию merge класса MapUtilsImpl

--- src/practic/task5/MapUtilsImpl.java
+++ src/practic/task5/MapUtilsImpl.java
@@ -1,6 +1,8 @@
 package practic.task5;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import nedis.study.interfaces.t5.collections.MapKey;
 import nedis.study.interfaces.t5.collections.MapUtils;
@@ -8,11 +10,26 @@ import nedis.study.interfaces.t5.collections.MultiMap;
 
 public class MapUtilsImpl implements MapUtils {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public MultiMap<MapKey, Integer> merge(Map<MapKey, Integer>... maps)
 			throws IllegalArgumentException {
-		maps[0].keySet();
-		return null;
+		
+		for (int i = 0; i < maps.length; i++) {
+			if (maps[i] == null) {
+				throw new IllegalArgumentException();
+			}
+		}
+		
+		MultiMap result = new MultiMapImpl();
+		
+		for (int i = 0; i < maps.length; i++) {
+			for (Entry<MapKey, Integer> entry : maps[i].entrySet()) {
+				result.put((MapKeyImpl) entry.getKey(), entry.getValue());
+			}
+		}
+		
+		return result;
 	}
 
 }
