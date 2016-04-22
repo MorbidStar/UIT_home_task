cd990b4bb66dc50dc25b28beb111121b7bc9b102
MorbidStar <morbidforever1@rambler.ru>
2016-04-22 11:59:30 +0300

    add logback.xml и создал логер в FindFilesTaskImpl

--- src/practic/task7/FindFilesTaskImpl.java
+++ src/practic/task7/FindFilesTaskImpl.java
@@ -3,16 +3,17 @@ package practic.task7;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import practic.task6.IOUtilsImpl;
-import nedis.study.interfaces.t6.io.IOUtils;
 import nedis.study.interfaces.t7.threads.FindFilesTask;
-import nedis.study.interfaces.t7.threads.Task;
 import nedis.study.interfaces.t7.threads.TaskExecutionFailedException;
 
 public class FindFilesTaskImpl extends TaskImpl implements FindFilesTask {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(FindFilesTaskImpl.class);
 	private String directory;
 	private String searchString;
 	private PrintStream out;
@@ -33,6 +34,7 @@ public class FindFilesTaskImpl extends TaskImpl implements FindFilesTask {
 		}
 		
 		this.directory = directory;
+		LOGGER.debug("директория установлена");
 	}
 
 	@Override
@@ -52,7 +54,7 @@ public class FindFilesTaskImpl extends TaskImpl implements FindFilesTask {
 
 	@Override
 	public void execute() throws TaskExecutionFailedException {
-		
+		LOGGER.debug("start task");
 		try {
 			iou = new IOUtilsImpl();
 			String[] res = iou.findFiles(directory, searchString);
@@ -63,6 +65,7 @@ public class FindFilesTaskImpl extends TaskImpl implements FindFilesTask {
 		} catch (Exception e) {
 			throw new TaskExecutionFailedException("задача выполнена некорректно", e);
 		}
+		LOGGER.debug("end task");
 	}
 
 }
